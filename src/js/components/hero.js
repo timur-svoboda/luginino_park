3d12f16d2d8665786b878c1b98876a430c98b2ab
Timur Svoboda <svobodatimur@gmail.com>
2022-05-24 16:55:17 +0300

    feat(hero): animate title

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,4 +1,24 @@
+import gsap from 'gsap/all';
+import SplitText from '../libs/SplitText.min';
+
 export default function hero() {
+	const $heroTopTitle = document.querySelector('.heroTopTitle');
+
+	if ($heroTopTitle) {
+		const titleSplitedText = new SplitText('.heroTopTitle');
+
+		gsap.from(titleSplitedText.chars, {
+			stagger: 0.05,
+			rotate: 180,
+			y: '100%',
+			opacity: 0,
+			scale: 0,
+			onComplete: () => {
+				titleSplitedText.revert();
+			},
+		});
+	}
+
 	const $checkboxContainer = document.querySelector('.heroCheckboxContainer');
 
 	if ($checkboxContainer) {
